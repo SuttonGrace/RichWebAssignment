942ef2db72c6f6b4744b086f5df327e42e34e1f0
Grace <grace.sutton@student.dit.ie>
2015-10-31 13:16:46 +0000

    Final

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function dispAbt(){
-			$('.box').removeClass('box-exp box-int box-awa box-skill');
-			$('.box').addClass('box-abt');
+			var element = document.getElementById('box');
+			element.className = 'box box-abt';
 			
 			document.getElementById('abt-txt').style.display='block';
 			$('.abt-img').show();
@@ -17,11 +17,16 @@ function dispAbt(){
 			document.getElementById('int-txt').style.display='none';
 			$('.int-img').hide();
 			
+			var elements = document.getElementsByClassName('btn'), i;
+			for (var i=0; i < elements.length; i++){
+				elements[i].className = 'btn btn-abt animated fadeIn';
+			}
+			
 		};
 		
 		function dispExp(){
-			$('.box').removeClass('box-abt box-int box-awa box-skill');
-			$('.box').addClass('box-exp');
+			var element = document.getElementById('box');
+			element.className = 'box box-exp';
 			
 			document.getElementById('abt-txt').style.display='none';
 			$('.abt-img').hide();
@@ -37,11 +42,17 @@ function dispAbt(){
 			
 			document.getElementById('int-txt').style.display='none';
 			$('.int-img').hide();
+			
+			var elements = document.getElementsByClassName('btn'), i;
+			for (var i=0; i < elements.length; i++){
+				elements[i].className = 'btn btn-exp animated fadeIn';
+			}
+			
 		};
 		
 		function dispAwa(){
-			$('.box').removeClass('box-abt box-int box-exp box-skill');
-			$('.box').addClass('box-awa');
+			var element = document.getElementById('box');
+			element.className = 'box box-awa';
 		
 			document.getElementById('abt-txt').style.display='none';
 			$('.abt-img').hide();
@@ -57,11 +68,16 @@ function dispAbt(){
 			
 			document.getElementById('int-txt').style.display='none';
 			$('.int-img').hide();
+			
+			var elements = document.getElementsByClassName('btn'), i;
+			for (var i=0; i < elements.length; i++){
+				elements[i].className = 'btn btn-awa animated fadeIn';
+			}
 		};
 		
 		function dispSkill(){
-			$('.box').removeClass('box-abt box-int box-exp box-awa');
-			$('.box').addClass('box-skill');	
+			var element = document.getElementById('box');
+			element.className = 'box box-skill';	
 			
 			document.getElementById('abt-txt').style.display='none';
 			$('.abt-img').hide();
@@ -77,11 +93,16 @@ function dispAbt(){
 			
 			document.getElementById('int-txt').style.display='none';
 			$('.int-img').hide();
+			
+			var elements = document.getElementsByClassName('btn'), i;
+			for (var i=0; i < elements.length; i++){
+				elements[i].className = 'btn btn-skill animated fadeIn';
+			}
 		};
 		
 		function dispInt(){
-			$('.box').removeClass('box-abt box-exp box-awa box-skill');
-			$('.box').addClass('box-int');
+			var element = document.getElementById('box');
+			element.className = 'box box-int';
 		
 			document.getElementById('abt-txt').style.display='none';
 			$('.abt-img').hide();
@@ -97,6 +118,11 @@ function dispAbt(){
 			
 			document.getElementById('int-txt').style.display='block';
 			$('.int-img').show();
+			
+			var elements = document.getElementsByClassName('btn'), i;
+			for (var i=0; i < elements.length; i++){
+				elements[i].className = 'btn btn-int animated fadeIn';
+			}
 		};
 
 
